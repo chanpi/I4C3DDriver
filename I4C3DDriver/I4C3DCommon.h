c498f5996f860bf9948b7e7d81f82e33aa278fab
chanpi <teemoco@gmail.com>
2012-03-24 16:32:03 +0900

    add registermacro message

--- I4C3DDriver/I4C3DCommon.h
+++ I4C3DDriver/I4C3DCommon.h
@@ -11,6 +11,10 @@
 // init Alt+Shift 1.0 1.0 0.5 ?\0
 static const char* g_initCommandFormat = "%s %s %lf %lf %lf %c";
 
+// registermacro [マクロ名] [キーの組み合わせ] [終端文字]\0
+// registermacro Macro1 1?\0
+static const char* g_registerMacroFormat = "%s %s %s %c";
+
 typedef struct {
 	unsigned char hwnd[4];
 	char szCommand[I4C3D_RECEIVE_LENGTH];
@@ -19,5 +23,11 @@ typedef struct {
 const int I4C3DAlpha = 1;
 const int cancelKeyDownMillisec = 1000;
 const int waitModkeyDownCount	= 100;
+
+class IRegisterMacro
+{
+	virtual LPCSTR RegisterMacro(LPCSTR szBuffer, char* termination) = 0;
+	virtual void PlayMacro(LPCSTR macroName) = 0;
+};
 
 #endif	/* _I4C3DCOMMON_H_ */
